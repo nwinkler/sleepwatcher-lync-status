bbbfeea5a2fee0139bda363f8b7993ae6d5e8f0e
Nils Winkler <nils@nilswinkler.com>
2014-12-05 17:38:48 +0100

    Using store/restore scripts on dim/wakeup.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ launchctl unload ~/Library/LaunchAgents/com.nilswinkler.sleepwatcher-lync-status
 
 ln -sfv $SCRIPT_DIR/com.nilswinkler.sleepwatcher-lync-status-20compatibility-localuser.plist ~/Library/LaunchAgents/
 
-ln -s $SCRIPT_DIR/lync-away.sh ~/.sleepwatcher-dim
-ln -s $SCRIPT_DIR/lync-reset.sh ~/.sleepwatcher-wakeup
+ln -s $SCRIPT_DIR/lync-store-away.sh ~/.sleepwatcher-dim
+ln -s $SCRIPT_DIR/lync-restore-status.sh ~/.sleepwatcher-wakeup
 
 launchctl load ~/Library/LaunchAgents/com.nilswinkler.sleepwatcher-lync-status-20compatibility-localuser.plist
