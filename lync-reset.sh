769dc4bc1c67d314a21508ff9d6cb7d7b37fc258
Nils Winkler <nils@nilswinkler.com>
2014-12-05 09:58:10 +0100

    Fixed script location, now also works for symbolic links.

--- lync-reset.sh
+++ lync-reset.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-source $(dirname $0)/_lync-status.sh
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+while([ -h "${SCRIPT_PATH}" ]); do
+  cd "`dirname "${SCRIPT_PATH}"`"
+  SCRIPT_PATH="$(readlink "`basename "${SCRIPT_PATH}"`")";
+done
+cd "`dirname "${SCRIPT_PATH}"`" > /dev/null
+SCRIPT_PATH="`pwd`";
+popd  > /dev/null
+
+source $SCRIPT_PATH/_lync-status.sh
 
 _lync-status "Reset Status"
