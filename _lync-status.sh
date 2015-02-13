6bda3940c9dbad933b6d457220c7e8dab95b3249
Nils Winkler <nils@nilswinkler.com>
2015-02-13 16:27:24 +0000

    Added check for running Lync instance.
    If Lync is not running, it will not be started, which was happening with
    the previous version. The status will now only be stored/restored if
    Lync is already running.

--- _lync-status.sh
+++ _lync-status.sh
@@ -11,33 +11,35 @@ function _syslog-sleepwatcher() {
 
 function _lync-status() {
   osascript 2>/dev/null <<EOF
-  -- Save your current application
-  tell application "System Events"
-    set currentApp to name of 1st process whose frontmost is true
-  end tell
+  if application "Microsoft Lync" is running then
+    -- Save your current application
+    tell application "System Events"
+      set currentApp to name of 1st process whose frontmost is true
+    end tell
 
-  -- Bring Lync to the front so we can use the menu
-  tell application "Microsoft Lync"
-    activate
-  end tell
+    -- Bring Lync to the front so we can use the menu
+    tell application "Microsoft Lync"
+      activate
+    end tell
 
-  -- Set your status to the provided parameter
-  tell application "System Events"
-    tell process "Microsoft Lync"
-      tell menu bar 1
-        tell menu bar item "Status"
-          tell menu "Status"
-            click menu item "$1"
+    -- Set your status to the provided parameter
+    tell application "System Events"
+      tell process "Microsoft Lync"
+        tell menu bar 1
+          tell menu bar item "Status"
+            tell menu "Status"
+              click menu item "$1"
+            end tell
           end tell
         end tell
       end tell
     end tell
-  end tell
 
-  -- Return to your previous application
-  tell application currentApp
-    activate
-  end tell
+    -- Return to your previous application
+    tell application currentApp
+      activate
+    end tell
+  end if
 EOF
 
 _syslog-sleepwatcher "$1"
@@ -45,46 +47,48 @@ _syslog-sleepwatcher "$1"
 
 function _lync-store-status() {
   osascript 2>/dev/null <<EOF
-  -- Save your current application
-  tell application "System Events"
-    set currentApp to name of 1st process whose frontmost is true
-  end tell
-
-  -- Bring Lync to the front so we can use the menu
-  tell application "Microsoft Lync"
-    activate
-  end tell
-
-  -- Store the status
-  tell application "System Events"
-    tell process "Microsoft Lync"
-      set statusMenu to menu bar item "Status" of menu bar 1
-      set allUiElements to entire contents of statusMenu
-
-      repeat with anElement in allUiElements
-        try
-          set checked to value of attribute "AXMenuItemMarkChar" of anElement
-
-          if checked is "✓" then
-            log checked
-
-            set menuItemName to name of anElement
-
-            log menuItemName
-
-            do shell script "echo " & quoted form of menuItemName & " > $STATUSFILE"
-
-            exit repeat
-          end if
-        end try
-      end repeat
+  if application "Microsoft Lync" is running then
+    -- Save your current application
+    tell application "System Events"
+      set currentApp to name of 1st process whose frontmost is true
     end tell
-  end tell
 
-  -- Return to your previous application
-  tell application currentApp
-    activate
-  end tell
+    -- Bring Lync to the front so we can use the menu
+    tell application "Microsoft Lync"
+      activate
+    end tell
+
+    -- Store the status
+    tell application "System Events"
+      tell process "Microsoft Lync"
+        set statusMenu to menu bar item "Status" of menu bar 1
+        set allUiElements to entire contents of statusMenu
+
+        repeat with anElement in allUiElements
+          try
+            set checked to value of attribute "AXMenuItemMarkChar" of anElement
+
+            if checked is "✓" then
+              log checked
+
+              set menuItemName to name of anElement
+
+              log menuItemName
+
+              do shell script "echo " & quoted form of menuItemName & " > $STATUSFILE"
+
+              exit repeat
+            end if
+          end try
+        end repeat
+      end tell
+    end tell
+
+    -- Return to your previous application
+    tell application currentApp
+      activate
+    end tell
+  end if
 EOF
 }
 
